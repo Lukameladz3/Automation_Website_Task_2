b5106c521d0bb39bbc738150cb817f3fbd2ec1b2
Lukameladz3 <lukameladz3@gmail.com>
2026-01-29 12:52:54 +0400

    added age optional parameter to users schema

--- src/usersapi/models/schemas/user.schemas.ts
+++ src/usersapi/models/schemas/user.schemas.ts
@@ -6,7 +6,7 @@ import { z } from "zod";
 export const UserSchema = z.object({
   id: z.number().int().positive(),
   name: z.string().min(1),
-  age: z.number().int().positive(),
+  age: z.number().int().positive().optional(),
 });
 
 export type User = z.infer<typeof UserSchema>;
